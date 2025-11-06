ff889979b76276d9582e3ab90346435ce5d3085b
Hung Anh <anhlhpl52119@gmail.com>
2025-11-07 06:06:55 +0700

    chore: add deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
+import { runtimeConfig } from './config/runtime';
+import { typescript } from './config/typescript';
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-
+  css: ['~/assets/css/app.css'],
   vite: {
     plugins: [tailwindcss()],
   },
   imports: {
     dirs: ['constants'],
   },
+  typescript,
+  runtimeConfig,
 });
