4016ef3c8976982fe85467454a533c58ece4a5fd
Hung Anh <anhlhpl52119@gmail.com>
2025-11-10 06:08:11 +0700

    feat: gnb

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -3,26 +3,29 @@ console.log('');
 </script>
 
 <template>
-  <header class="h-[64px] bg-abg-base flex px-10">
-    <div class="flex gap-2 items-center">
-      <div class="font-bold text-lg">
-        STOVE
+  <header class="h-[64px] bg-abg-base">
+    <nav class="max-w-[1200px] h-full px-10 mx-auto flex items-center justify-end-safe">
+      <!-- Left -->
+      <div class="mr-auto">
+        <strong class="text-lg">STOVE</strong>
+        <span class="inline-block ml-1">
+          창작자 센터 바로가기
+        </span>
       </div>
 
-      <p>
-        창작자 센터 바로가기
-      </p>
-    </div>
-
-    <div class="ml-auto flex items-center gap-2">
-      <div>창작자 센터 바로가기</div>
-      <input
-        type="text"
-        class="border border-abd-base rounded-full py-3 pl-4 pr-10 w-[220px]"
-        placeholder="검색어를 입력하세요."
-      >
-      <div>Locale</div>
-      <div>avatar</div>
-    </div>
+      <!-- Right -->
+      <ul class="flex gap-4 items-center">
+        <li><span class="inline-block">창작자 센터 바로가기</span></li>
+        <li>
+          <input
+            type="text"
+            class="border border-abd-base rounded-full py-3 pl-4 pr-10 w-[220px]"
+            placeholder="검색어를 입력하세요."
+          >
+        </li>
+        <li><i class="block">Locale</i></li>
+        <li><i class="block">avatar</i></li>
+      </ul>
+    </nav>
   </header>
 </template>
