c799ab21bc439408e5f2080135f6b8d9e42214cb
Lam Hung Anh (Brim) <lamhunganh.vn@smilegate.com>
2025-11-09 12:13:51 +0700

    style: update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,13 +8,23 @@ const config = useRuntimeConfig();
 
 <template>
   <main class="asdss">
-    <h1>Ok bro</h1>
+    <h1 class="text-base">
+      Ok bro
+    </h1>
     <div>
       <h1 class="font-bold">
         Runtime
       </h1>
       <pre class="mt-24">{{ config.public }}</pre>
     </div>
-    <span class="block mt-12 text-red-300">hello</span>
+    <span class="block mt-12">hello</span>
+    <div class="text-dimmed">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ad magnam rem necessitatibus, sint autem molestias ut possimus inventore, illo eius voluptatem nisi delectus mollitia illum aut officia debitis quisquam.
+        Ex sit ut atque porro exercitationem fugit laborum odio totam ab accusantium! Beatae rem a dolorem eum est ratione dignissimos. Delectus quod vel architecto, consequatur nam unde praesentium eligendi esse.
+      </p>
+      <br><br>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptas laboriosam distinctio dolorum recusandae consequuntur consequatur saepe, alias quos eveniet earum inventore, optio dolore nisi magnam iste vel. Id, odio?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto minima dolorem aut qui quibusdam pariatur incidunt, nisi laudantium facere necessitatibus magni consequatur numquam vero dolores alias saepe provident natus? Facere.
+    </div>
   </main>
 </template>
