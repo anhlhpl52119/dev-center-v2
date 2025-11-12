ada8367b2038e83e6c0b950924b9d551031a5ab2
Lam Hung Anh (Brim) <lamhunganh.vn@smilegate.com>
2025-11-12 11:12:55 +0700

    feat: design system

--- app/components/AppLNB.vue
+++ app/components/AppLNB.vue
@@ -3,20 +3,20 @@
     <Icon name="svg:menu" class="block size-10" />
 
     <ul class="mt-6">
-      <li class="rounded-full h-10 py-2 pl-4 pr-3">
-        <a href="#" class="font-bold text-base text-tcl-raised">
+      <li class="rounded-full py-2 pl-4 pr-3 text-md font-bold text-base text-tcl-raised leading-6">
+        <a href="#">
           <span>불카누스 가이드</span>
         </a>
       </li>
       <ul class="pl-4">
-        <li class="rounded-full font-medium h-10 py-2 pl-4 pr-3 bg-abg-active text-tcl-primary">
+        <li class="leading-6 rounded-full font-medium py-2 pl-4 pr-3 bg-abg-active text-tcl-primary">
           <a href="#"><span>불카누스 가이드</span></a>
         </li>
-        <li class="rounded-full font-medium h-10 py-2 pl-4 pr-3">
+        <li class="leading-6 rounded-full font-medium py-2 pl-4 pr-3">
           <a href="#"><span>설치와 실행</span></a>
         </li>
       </ul>
-      <li class="rounded-full font-medium text-tcl-raised h-10 py-2 pl-4 pr-3">
+      <li class="text-md leading-6 rounded-full font-medium text-tcl-raised py-2 pl-4 pr-3">
         <a href="#">
           <span>불카누스 메뉴2</span>
         </a>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,7 @@ definePageMeta({
 </script>
 
 <template>
-  <main class="flex min-h-screen max-w-7xl mx-auto relative">
+  <main class="flex max-w-7xl mx-auto relative">
     <AppLNB class="w-68 shrink-0 h-screen sticky top-0 overflow-y-auto p-6 pt-8 hidden md:block" />
 
     <AppArticle class="pt-10 pb-38.5 min-w-80" />
