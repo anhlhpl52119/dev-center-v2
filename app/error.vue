6479d95864bdb113fad3213b1c0633ec5e289e33
Hung Anh <anhlhpl52119@gmail.com>
2025-11-15 22:06:53 +0700

    update deps

---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const error = useError();
+</script>
+
+<template>
+  <main>
+    <h1>Error please back home</h1>
+    <pre>{{ error }}</pre>
+    <NuxtLinkLocale class="mt-10 text-primary" to="/">
+      Back home
+    </NuxtLinkLocale>
+  </main>
+</template>
